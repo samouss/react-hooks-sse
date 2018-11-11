ed261da05ec03ab0b72d8daf0dd63cbc46c79adc
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-11-11 00:09:35 -0500

    feat(SSEContext): use wrapper around EventSource

--- example/src/lib/SSEContext.js
+++ example/src/lib/SSEContext.js
@@ -1,15 +1,56 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 // import { string, boolean, shape } from 'prop-types';
 
 export const SSEContext = createContext(null);
 
 export const SSEConsumer = SSEContext.Consumer;
 
+const createEventSourceManager = ({ endpoint, options }) => {
+  const state = {
+    source: null,
+    listenersByName: new Map(),
+  };
+
+  return {
+    addEventListener(name, listener) {
+      if (!state.listenersByName.size) {
+        state.source = new EventSource(endpoint, options);
+      }
+
+      const listeners = state.listenersByName.get(name) || new Set();
+
+      listeners.add(listener);
+
+      state.listenersByName.set(name, listeners);
+
+      state.source.addEventListener(name, listener);
+    },
+    removeEventListener(name, listener) {
+      const listeners = state.listenersByName.get(name) || new Set();
+
+      listeners.delete(listener);
+
+      if (!listeners.size) {
+        state.listenersByName.delete(name);
+      }
+
+      state.source.removeEventListener(name, listener);
+
+      if (!state.listenersByName.size) {
+        state.source.close();
+        state.source = null;
+      }
+    },
+  };
+};
+
 export const SSEProvider = ({ endpoint, options, ...props }) => {
-  // @TODO: enable one time created event
-  // Create a new source on each render
-  // - useEffect with state
-  const source = new EventSource(endpoint, options);
+  const [source] = useState(() =>
+    createEventSourceManager({
+      endpoint,
+      options,
+    })
+  );
 
   return <SSEContext.Provider {...props} value={source} />;
 };
