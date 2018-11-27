2648078d37bf0787fb2a67f79d9d29de62353870
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-11-27 21:10:36 +0100

    chore: move example/src/lib to src

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
-import { useState, useEffect } from 'react';
-
-const useSSE = (endpoint, params) => {
-  const [value, setValue] = useState([]);
-
-  useEffect(() => {
-    const source = new EventSource(endpoint, params);
-
-    source.addEventListener('time', event => {
-      const data = JSON.parse(event.data);
-
-      setValue(value.concat(data));
-    });
-
-    return () => {
-      source.close();
-    };
-  }, []);
-
-  return value;
-};
-
-export default useSSE;
+export * from './SSEContext';
+export * from './useSSESubscription';
