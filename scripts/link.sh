bbc3e711b6c539811b695ab55d5284fc1a437800
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-12-02 18:42:22 +0100

    chore: setup scripts to link packages

---
+++ scripts/link.sh
@@ -0,0 +1,3 @@
+(cd node_modules/react && yarn link)
+yarn link
+(cd example && yarn && yarn link react-hook-sse && yarn link react)
---
+++ scripts/unlink.sh
@@ -0,0 +1,3 @@
+(cd example && yarn unlink react-hook-sse && yarn unlink react)
+yarn unlink
+(cd node_modules/react && yarn unlink)
