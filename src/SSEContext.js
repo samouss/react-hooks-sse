7287b180fcde9de043254e68ed36534bc9949bf9
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-12-09 18:43:59 +0100

    feat(Context): implement SSEProvider

--- src/SSEContext.js
+++ src/SSEContext.js
@@ -1,48 +1,10 @@
 import { createElement, createContext, useState } from 'react';
-// import { string, boolean, shape } from 'prop-types';
+import { string, bool, shape } from 'prop-types';
+import { createEventSourceManager } from './createEventSourceManager';
 
 export const SSEContext = createContext(null);
 
 export const SSEConsumer = SSEContext.Consumer;
-
-const createEventSourceManager = ({ endpoint, options }) => {
-  const state = {
-    source: null,
-    listenersByName: new Map(),
-  };
-
-  return {
-    addEventListener(name, listener) {
-      if (!state.listenersByName.size) {
-        state.source = new EventSource(endpoint, options);
-      }
-
-      const listeners = state.listenersByName.get(name) || new Set();
-
-      listeners.add(listener);
-
-      state.listenersByName.set(name, listeners);
-
-      state.source.addEventListener(name, listener);
-    },
-    removeEventListener(name, listener) {
-      const listeners = state.listenersByName.get(name) || new Set();
-
-      listeners.delete(listener);
-
-      if (!listeners.size) {
-        state.listenersByName.delete(name);
-      }
-
-      state.source.removeEventListener(name, listener);
-
-      if (!state.listenersByName.size) {
-        state.source.close();
-        state.source = null;
-      }
-    },
-  };
-};
 
 export const SSEProvider = ({ endpoint, options, ...props }) => {
   const [source] = useState(() =>
@@ -58,16 +20,15 @@ export const SSEProvider = ({ endpoint, options, ...props }) => {
   });
 };
 
-// @TODO: enable props
-// SSEProvider.propTypes = {
-//   endpoint: string.isRequired,
-//   options: shape({
-//     withCredentials: boolean.isRequired,
-//   }),
-// };
+SSEProvider.propTypes = {
+  endpoint: string.isRequired,
+  options: shape({
+    withCredentials: bool.isRequired,
+  }),
+};
 
-// SSEProvider.defaultProps = {
-//   options: {
-//     withCredentials: false,
-//   },
-// };
+SSEProvider.defaultProps = {
+  options: {
+    withCredentials: false,
+  },
+};
--- src/__tests__/SSEContext.js
+++ src/__tests__/SSEContext.js
@@ -1,5 +1,86 @@
-import { SSEContext } from '../SSEContext';
+import { createElement } from 'react';
+import TestRenderer from 'react-test-renderer';
+import { SSEProvider, SSEConsumer } from '../SSEContext';
+import { createEventSourceManager } from '../createEventSourceManager';
 
-it('expect to be true', () => {
-  expect(SSEContext).toBeTruthy();
+jest.mock('../createEventSourceManager', () => ({
+  createEventSourceManager: jest.fn(),
+}));
+
+describe('SSEContext', () => {
+  const defaultOptions = {
+    endpoint: 'http://localhost/sse',
+  };
+
+  beforeEach(() => {
+    createEventSourceManager.mockReset();
+  });
+
+  it('expect to create a source manager with default options', () => {
+    TestRenderer.create(
+      createElement(SSEProvider, {
+        ...defaultOptions,
+      })
+    );
+
+    expect(createEventSourceManager).toHaveBeenCalledWith({
+      endpoint: 'http://localhost/sse',
+      options: {
+        withCredentials: false,
+      },
+    });
+  });
+
+  it('expect to create a source manager with provided options', () => {
+    TestRenderer.create(
+      createElement(SSEProvider, {
+        ...defaultOptions,
+        options: {
+          withCredentials: true,
+        },
+      })
+    );
+
+    expect(createEventSourceManager).toHaveBeenCalledWith({
+      endpoint: 'http://localhost/sse',
+      options: {
+        withCredentials: true,
+      },
+    });
+  });
+
+  it('expect to create a source manager only once across render', () => {
+    const createProviderElement = () =>
+      createElement(SSEProvider, {
+        ...defaultOptions,
+      });
+
+    const renderer = TestRenderer.create(createProviderElement());
+
+    expect(createEventSourceManager).toHaveBeenCalledTimes(1);
+
+    renderer.update(createProviderElement());
+    renderer.update(createProviderElement());
+    renderer.update(createProviderElement());
+
+    expect(createEventSourceManager).toHaveBeenCalledTimes(1);
+  });
+
+  it('expect to expose a source manager through the context', () => {
+    const manager = {};
+
+    createEventSourceManager.mockImplementationOnce(() => manager);
+
+    TestRenderer.create(
+      createElement(
+        SSEProvider,
+        {
+          ...defaultOptions,
+        },
+        createElement(SSEConsumer, {}, exposed => {
+          expect(exposed).toBe(manager);
+        })
+      )
+    );
+  });
 });
