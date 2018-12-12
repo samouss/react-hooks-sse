b3b7676582adb3e8d33b3fa64b168e546a13a81e
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-12-12 20:26:29 +0100

    test(useSSE): cover subscription lifecycle

--- src/__tests__/ussSSE.test.js
+++ src/__tests__/ussSSE.test.js
@@ -58,4 +58,65 @@ describe('useSSE', () => {
       'Hello World from SSE with "earth"',
     ]);
   });
+
+  describe('subscription', () => {
+    it('expect to register a listener on mount', () => {
+      const source = createFakeSource();
+      const context = createFakeContext(source);
+
+      TestRenderer.create(
+        createElement(FakeApp, {
+          context,
+        })
+      );
+
+      expect(source.addEventListener).toHaveBeenCalledTimes(1);
+      expect(source.addEventListener).toHaveBeenCalledWith(
+        'push',
+        expect.any(Function)
+      );
+    });
+
+    it('expect to not register a listener on update', () => {
+      const source = createFakeSource();
+      const context = createFakeContext(source);
+      const mockCreateElement = jest.fn(() =>
+        createElement(FakeApp, {
+          context,
+        })
+      );
+
+      const createFakeApp = () => mockCreateElement();
+
+      const renderer = TestRenderer.create(createFakeApp());
+
+      expect(mockCreateElement).toHaveBeenCalledTimes(1);
+      expect(source.addEventListener).toHaveBeenCalledTimes(1);
+
+      renderer.update(createFakeApp());
+      renderer.update(createFakeApp());
+      renderer.update(createFakeApp());
+
+      expect(mockCreateElement).toHaveBeenCalledTimes(4);
+      expect(source.addEventListener).toHaveBeenCalledTimes(1);
+    });
+
+    it('expect to remove the listener on unmount', () => {
+      const source = createFakeSource();
+      const context = createFakeContext(source);
+      const renderer = TestRenderer.create(
+        createElement(FakeApp, {
+          context,
+        })
+      );
+
+      renderer.unmount();
+
+      expect(source.removeEventListener).toHaveBeenCalledTimes(1);
+      expect(source.removeEventListener).toHaveBeenCalledWith(
+        'push',
+        expect.any(Function)
+      );
+    });
+  });
 });
