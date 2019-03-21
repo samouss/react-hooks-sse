a6f63f2004c83cacdaf85bdaf71acf8364bf6e3c
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-03-21 19:31:28 +0100

    test: drop useless React mock for act

--- src/__mocks__/react.js
+++
@@ -1,7 +0,0 @@
-const React = require.requireActual('react');
-
-module.exports = {
-  ...React,
-  // see: https://github.com/facebook/react/issues/14050#issuecomment-438173736
-  useEffect: React.useLayoutEffect,
-};
